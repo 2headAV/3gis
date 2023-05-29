bd0234a669152ce1c7e7d7a240c16a9bd4b28a6e
Alla Artem <artemav.3701@gmail.com>
2023-05-29 22:52:03 +0700

    add

--- src/db.ts
+++ src/db.ts
@@ -14,5 +14,12 @@ export const data = [
       adress: 'adressadressadressadressadressadressad ressadressadressadressadressadress',
       img: lenin,
       descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore'
+   },
+   {
+      coord: [92.804156, 56.00792],
+      title: 'ydarnik',
+      adress: 'adressadressadressadressadressadressad ressadressadressadressadressadress',
+      img: lenin,
+      descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore'
    }
 ];
