4176f4a6747b268e82f726b86dbf7fad8978352e
qwe <carandash29@gmail.com>
2023-06-02 18:50:00 +0700

    qwe

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,21 @@
 import lenin from '../images/LENIN.jpg';
+import larionovs from '../images/LENIN.jpg';
+
 
 export const data = [
    {
       coord: [92.877934, 56.015396],
-      title: 'lox',
-      adress: 'adressadressadressadressadressadressad ressadressadressadressadressadress',
+      title: 'Памятная доска В.И. Ленину',
+      adress: 'Марковского, 27 ',
       img: lenin,
-      descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore'
+      descr: 'В этом доме Владимир Ильич Ленин Работал в марте - апреле 1897 г.'
    },
    {
-      coord: [92.804156, 56.007981],
-      title: 'ydarnik',
+      coord: [92.889872, 56.014402],
+      title: 'Дом жилой 1820-1830 гг.',
       adress: 'adressadressadressadressadressadressad ressadressadressadressadressadress',
-      img: lenin,
-      descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore'
+      img: larionovs,
+      descr: 'Дом жилой с воротами усадьбы Ларионовых 1820-1830 гг.'
    },
    {
       coord: [92.804156, 56.00792],
